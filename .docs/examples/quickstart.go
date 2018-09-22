05d3ea4ed86c8d65738a29349a63b06e3beafc46
nmrshll <marshall.nicolas@gmail.com>
2018-09-22 14:23:33 +0200

    add example

--- .docs/examples/quickstart.go
+++ .docs/examples/quickstart.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	middl "github.com/nmrshll/go-httpclient-middl"
+	"github.com/nmrshll/go-httpclient-middl/middleware/logger"
+	"github.com/nmrshll/go-httpclient-middl/middleware/statusvalidator"
+)
 
 func main() {
-	fmt.Println("Hello world !")
+	httpClient := http.Client{Timeout: 30 * time.Second}
+	client, err := middl.NewClient(&httpClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// add middleware to you client (classic examples provided in this library or custom)
+	client.UseMiddleware(logger.New())
+	client.UseMiddleware(statusvalidator.New())
+
+	// then do your requests as usual
+	resp, err := client.Get("https://google.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp == nil {
+		log.Fatalf("no response from server")
+	} // else
+	defer resp.Body.Close()
+
+	// do something with the response here
 }
